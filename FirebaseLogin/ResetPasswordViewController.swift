d880049bcf4a926bfabf3fbceba8fa963affbbe6
pritam hinger <pritamgenius24@gmail.com>
2017-01-15 00:15:35 +0530

    Add reset password functionality.

--- FirebaseLogin/ResetPasswordViewController.swift
+++ FirebaseLogin/ResetPasswordViewController.swift
@@ -7,9 +7,12 @@
 //
 
 import UIKit
+import Firebase
+import FirebaseAuth
 
 class ResetPasswordViewController: UIViewController {
 
+    @IBOutlet weak var emailTextField: UITextField!
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,6 +24,34 @@ class ResetPasswordViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func resetPassword(_ sender: Any) {
+        if emailTextField.text == ""{
+            let alertController = UIAlertController(title: "Oops!", message: "Please enter an email address", preferredStyle: .alert)
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(defaultAction)
+            present(alertController, animated: true, completion: nil)
+        }
+        else{
+            FIRAuth.auth()?.sendPasswordReset(withEmail: emailTextField.text!){ (error) in
+                var title = ""
+                var message = ""
+                if error != nil{
+                    title = "Error"
+                    message = (error?.localizedDescription)!
+                }
+                else{
+                    title = "Success!"
+                    message = "Password reset mail set to \(self.emailTextField.text!)"
+                    self.emailTextField.text = ""
+                }
+                
+                let alertController = UIAlertController(title: title, message: message, preferredStyle: .alert)
+                let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                alertController.addAction(defaultAction)
+                self.present(alertController, animated: true, completion: nil)
+            }
+        }
+    }
 
     /*
     // MARK: - Navigation
